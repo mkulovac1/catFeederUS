3f39e083ef23f23ffdeabaf3f9a4414da9ddc225
Merim Kulovac <mkulovac1@etf.unsa.ba>
2023-07-03 05:49:32 +0200

    v1

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
+import time
+import network
+import BlynkLib
 from ili934xnew import ILI9341, color565
 import machine
 from machine import Pin, SPI, PWM
 from micropython import const
-import time
 import os
-import glcdfont
-import tt14
-import tt24
 import tt32
-import time
 
-#crvena 3.3, smedja masa, narandzasta PWM 2000-5000 duty
+g = Pin(12, Pin.OUT)
+b = Pin(13, Pin.OUT)
+r = Pin(14, Pin.OUT)
 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect('Ugradbeni','USlaboratorija220')
 
-# Dimenzije displeja
+BLYNK_AUTH = "nK-MSkYJt4l3FPn8wX7cbCFu0YOooWBi"
+  
+wait = 10
+while wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    ip=wlan.ifconfig()[0]
+    print('IP: ', ip)
+ 
+"Connection to Blynk"
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
 SCR_WIDTH = const(320)
 SCR_HEIGHT = const(240)
 SCR_ROT = const(2)
 CENTER_Y = int(SCR_WIDTH/2)
 CENTER_X = int(SCR_HEIGHT/2)
 
-# Podešenja SPI komunikacije sa displejem
 TFT_CLK_PIN = const(18)
 TFT_MOSI_PIN = const(19)
 TFT_MISO_PIN = const(16)
 TFT_CS_PIN = const(17)
 TFT_RST_PIN = const(20)
 TFT_DC_PIN = const(15)
-
-# Fontovi na raspolaganju
-fonts = [glcdfont,tt14,tt24,tt32]
 
 spi = SPI(
     0,
@@ -49,80 +67,38 @@ display = ILI9341(
 
 display.set_font(tt32)
 
+correctPIN = 2306
+userPIN = ""
 
 def prikaziPocetniDisplej():
+    global userPIN, display
     display.erase()
-    # Različita orijentacija teksta na displeju
     display.set_pos(45,30)
     display.rotation=0
     display.print('Unesite PIN:')
-
+    userPIN=""
     display.set_pos(70,100)
     display.print('_ _ _ _')
 
-    # Offset za prikaz slike
-    offset_x = 160
-    offset_y = 0
-
-
-    # Učitavanje slike iz BMP fajla
-    f=open('macka3.bmp', 'rb')
-
-    # Prikaz piksela slike na displeju
-    if f.read(2) == b'BM':  #header
-        dummy = f.read(8) #file size(4), creator bytes(4)
-        offset = int.from_bytes(f.read(4), 'little')
-        hdrsize = int.from_bytes(f.read(4), 'little')
-        width = int.from_bytes(f.read(4), 'little')
-        height = int.from_bytes(f.read(4), 'little')
-        if int.from_bytes(f.read(2), 'little') == 1: #planes must be 1
-            depth = int.from_bytes(f.read(2), 'little')
-            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:#compress method == uncompressed
-                print("Image size:", width, "x", height)
-                rowsize = (width * 3 + 3) & ~3
-                if height < 0:
-                    height = -height
-                    flip = False
-                else:
-                    flip = True
-                w, h = width, height
-                for row in range(h):
-                    if flip:
-                        pos = offset + (height - 1 - row) * rowsize
-                    else:
-                        pos = offset + row * rowsize
-                    if f.tell() != pos:
-                        dummy = f.seek(pos)
-                    for col in range(w):
-                        bgr = f.read(3)
-                        display.pixel(offset_y+col,offset_x+row,color565(bgr[2],bgr[1],bgr[0]))
-
-
 def prikaziDisplejZaTacanPin():
-    print("Tacan")
+    global display
     display.erase()
     display.set_pos(35,20)
     display.rotation=0
     display.print('Ispravan PIN')
-    userPIN = ""
-           
-    # Offset za prikaz slike
+    
     offset_x = 65
     offset_y = 30
-
-    # Učitavanje slike iz BMP fajla
-    f=open('tacan(2).bmp', 'rb')
-
-    # Prikaz piksela slike na displeju
-    if f.read(2) == b'BM':  #header
-        dummy = f.read(8) #file size(4), creator bytes(4)
+    f=open('ispravan.bmp', 'rb')
+    if f.read(2) == b'BM':
+        dummy = f.read(8)
         offset = int.from_bytes(f.read(4), 'little')
         hdrsize = int.from_bytes(f.read(4), 'little')
         width = int.from_bytes(f.read(4), 'little')
         height = int.from_bytes(f.read(4), 'little')
-        if int.from_bytes(f.read(2), 'little') == 1: #planes must be 1
+        if int.from_bytes(f.read(2), 'little') == 1:
             depth = int.from_bytes(f.read(2), 'little')
-            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:#compress method == uncompressed
+            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:
                 rowsize = (width * 3 + 3) & ~3
                 if height < 0:
                     height = -height
@@ -140,34 +116,30 @@ def prikaziDisplejZaTacanPin():
                     for col in range(w):
                         bgr = f.read(3)
                         display.pixel(offset_y+col,offset_x+row,color565(bgr[2],bgr[1],bgr[0]))
-     
+        motor.duty_u16(5000)
+        time.sleep(0.3)
+        motor.duty_u16(2000)
 
 def prikaziDisplejZaNetacanPin():
-    print("Netacan")
+    global userPIN, display
     userPIN = ""
-    wrongPinCounter = 0
     display.erase()
     display.set_pos(25,20)
     display.rotation=0
     display.print('Neispravan PIN')
-    userPIN = ""
-    # Offset za prikaz slike
+    
     offset_x = 85
     offset_y = 25
-
-    # Učitavanje slike iz BMP fajla
-    f=open('neee.bmp', 'rb')
-
-    # Prikaz piksela slike na displeju
-    if f.read(2) == b'BM':  #header
-        dummy = f.read(8) #file size(4), creator bytes(4)
+    f=open('neispravan.bmp', 'rb')
+    if f.read(2) == b'BM':
+        dummy = f.read(8)
         offset = int.from_bytes(f.read(4), 'little')
         hdrsize = int.from_bytes(f.read(4), 'little')
         width = int.from_bytes(f.read(4), 'little')
         height = int.from_bytes(f.read(4), 'little')
-        if int.from_bytes(f.read(2), 'little') == 1: #planes must be 1
+        if int.from_bytes(f.read(2), 'little') == 1:
             depth = int.from_bytes(f.read(2), 'little')
-            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:#compress method == uncompressed
+            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:
                 rowsize = (width * 3 + 3) & ~3
                 if height < 0:
                     height = -height
@@ -185,9 +157,12 @@ def prikaziDisplejZaNetacanPin():
                     for col in range(w):
                         bgr = f.read(3)
                         display.pixel(offset_y+col,offset_x+row,color565(bgr[2],bgr[1],bgr[0]))
-    prikaziPocetniDisplej()
      
 prikaziPocetniDisplej()
+
+motor = PWM(Pin(28))
+motor.freq(50)
+motor.duty_u16(2000)
 
 R1 = Pin(21, Pin.OUT)
 R2 = Pin(22, Pin.OUT)
@@ -201,15 +176,8 @@ C4 = Pin(3, Pin.IN, Pin.PULL_DOWN)
 row = [R1, R2, R3, R4]
 column = [C1, C2, C3, C4]
 
-counter = 0
-
 for i in range(0,4):
     row[i].value(0)
-
-counter = 0
-isPressed = False
-autoIncrement = False
-autoTimer = 0.
 
 keyMatrix = [
     ["1", "2", "3", "A"],
@@ -217,11 +185,7 @@ keyMatrix = [
     ["7", "8", "9", "C"],
     ["*", "0", "#", "D"]]
 
-correctPIN = 2306
-userPIN = ""
-wrongPinCounter = 0
 isKey = False
-secondCounter = 0
 
 def getPressedKey(rows, columns):
     global isKey
@@ -237,7 +201,29 @@ def getPressedKey(rows, columns):
 varijabla=43
 brojac=0
 
-while 1:
+@blynk.on("V0") 
+def v0_write_handler(value): 
+    if int(value[0]) == 1:
+        print("ON")
+        motor.duty_u16(5000)
+        r.on()
+        b.on()
+        g.off()
+        time.sleep(0.3)
+        motor.duty_u16(2000)
+        r.off()
+        b.on()
+        g.on()
+    else:
+        print("OFF")
+        r.on()
+        b.off()
+        g.off()
+        motor.deinit()
+        time.sleep(1)
+
+while True:
+    blynk.run()
     key = getPressedKey(row, column)
     if key != None and key != "#":
         if key != "*" and brojac<4:
@@ -256,11 +242,15 @@ while 1:
             userPIN = userPIN + key
             brojac = brojac + 1
     elif key == "*" and len(userPIN)>0:
-        userPIN = userPIN[:-1]
-       
+        userPIN = userPIN[:-1]      
     elif key == "#":
         if len(userPIN) < 4 or int(userPIN) != correctPIN:
+            userPin = ""
             prikaziDisplejZaNetacanPin()
+            time.sleep(0.5)
+            brojac = 0
+            varijabla=43
+            prikaziPocetniDisplej()
         else:
             prikaziDisplejZaTacanPin()
     else:
